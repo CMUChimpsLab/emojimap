df802b929e753d70aabe4a838a061ff45aec4930
Dan Tasse <dan.tasse@gmail.com>
2015-03-02 13:35:33 -0500

    rename file, and now get_emoji works.

--- find_all_emoji.py
+++ find_all_emoji.py
@@ -1,9 +1,11 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
+# Python3 only! Because it can handle unicode astral characters.
 
-# Looping over all tweets in a collection is expensive, so let's try to only do
+# Loops over all tweets in the mongodb, makes a json file that is a list of
+# minimized tweets
 # it once.
 
-import argparse, pymongo, csv, datetime, json
+import argparse, pymongo, csv, datetime, json, re
 from collections import Counter
 
 # Returns a "shrunken" tweet that doesn't have a lot of the garbage that we
@@ -12,15 +14,25 @@ def shrink(tweet):
     new_tweet = {}
     for field in ['coordinates', 'created_at', 'id',
             'in_reply_to_status_id', 'in_reply_to_screen_name',
-            'lang', 'place', 'retweet_count', 'retweeted', 'source', 'text']:
+            'lang', 'retweet_count', 'retweeted', 'source', 'text']:
         new_tweet[field] = tweet[field]
     new_tweet['user'] = {'screen_name': tweet['user']['screen_name']}
     return new_tweet
 
-# Returns all the emoji in this string.
+emoji_symbols_pictograms = re.compile(u'[\U0001f300-\U0001f5fF]')
+emoji_emoticons = re.compile(u'[\U0001f600-\U0001f64F]')
+emoji_transport_maps = re.compile(u'[\U0001f680-\U0001f6FF]')
+emoji_symbols = re.compile(u'[\U00002600-\U000026FF]')
+emoji_dingbats = re.compile(u'[\U00002700-\U000027BF]')
+all_emoji = re.compile(u'([\U00002600-\U000027BF]|[\U0001f300-\U0001f64F]|[\U0001f680-\U0001f6FF])')
+
+# Returns all the emoji in this string. 'text' is a unicode string.
 def get_emoji(text):
-    return text
-    # for char in text:
+    emojis = all_emoji.findall(text)
+    if len(emojis) > 0:
+        return ','.join(emojis)
+    else:
+        return ''
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -37,17 +49,19 @@ if __name__ == '__main__':
     coll = db['tweet_' + args.city]
 
     emoji_tweets = []
-    counter = 0 # hah
     total = coll.count()
+    counter = 0
     for tweet in coll.find().limit(args.limit):
+        
         emoji = get_emoji(tweet['text'])
         if emoji != '':
             new_tweet = shrink(tweet)
-            # new_tweet['emoji'] = tweet['emoji']
+            new_tweet['emoji'] = emoji
             emoji_tweets.append(new_tweet)
 
         counter += 1
         if counter % 1000 == 0:
-            print 'Tweets processed: %d out of %d' % (counter, total)
+            print('Tweets processed: %d out of %d' % (counter, total))
 
+    print("Tweets: %d, emoji tweets: %d, percent with emoji: %.01f" % (total, len(emoji_tweets), len(emoji_tweets) * 100.0 / total))
     json.dump(emoji_tweets, open(args.outfile, 'w'), indent=2)
