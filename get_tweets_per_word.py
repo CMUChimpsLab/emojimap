7469aaea2f663a8d75951fbc24e4255bf5cd4e9d
Jennifer Chou <jennifertchou@gmail.com>
2015-06-20 01:42:08 +0000

    script to get context for words

---
+++ get_tweets_per_word.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+#For each neighborhood and each top 10 words for that neighborhood, 
+#compiles a list of tweets that contain that word.
+ 
+from collections import defaultdict
+import csv,sys
+from csv import DictWriter, DictReader
+import util.util, util.neighborhoods, cProfile
+from util.neighborhoods import get_neighborhood_or_muni_name
+import json
+import psycopg2, psycopg2.extras, ppygis
+import twokenize
+
+def run_all():
+
+    csv.field_size_limit(sys.maxsize)
+
+    psql_conn = psycopg2.connect("dbname='tweet'")
+    psycopg2.extras.register_hstore(psql_conn)
+    pg_cur = psql_conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    
+    # Build up the bins-to-nghds mapping so we can easily translate.
+    bins_to_nghds = {}
+    for line in DictReader(open('point_map.csv')):
+        bins_to_nghds[(float(line['lat']), float(line['lon']))] = line['nghd']
+
+    words_per_nghd = json.load(open('outputs/nghd_words_no_usernames.json'))
+    top10words = {}
+    tweets_per_word = defaultdict(lambda: defaultdict(list))
+   
+    for nghd in words_per_nghd:
+        top10words[nghd] = words_per_nghd[nghd]["top words"]
+
+    pg_cur.execute("SELECT text, ST_ASGEOJSON(coordinates), user_screen_name " + 
+                                                        "FROM tweet_pgh;")
+    counter = 0
+    for row in pg_cur:
+        counter += 1
+        if (counter % 10000) == 0:
+            print str(counter) + ' tweets processed'
+        coords = json.loads(row[1])['coordinates']
+        bin = util.util.round_latlon(coords[1], coords[0])
+        if bin in bins_to_nghds:
+            tweet_nghd = bins_to_nghds[bin]
+        else:
+            tweet_nghd = 'Outside Pittsburgh'
+        tweet = row[0]
+        username = row[2]
+        wordList = twokenize.tokenize(tweet)
+        for word in top10words[tweet_nghd]:
+            if word in wordList:
+                tweets_per_word[tweet_nghd][word].append(username + ": " + tweet)
+                
+            
+    '''
+    for nghd in words_per_nghd:
+        top10words[nghd] = words_per_nghd[nghd]["top words"]
+        for word in top10words[nghd]:
+            pg_cur.execute("SELECT text,ST_ASGEOJSON(coordinates) FROM tweet_pgh " +
+                                                "WHERE text LIKE '%%%s%%';" % (word,))
+                    
+            for row in pg_cur:   
+                coords = json.loads(row[1])['coordinates']
+                bin = util.util.round_latlon(coords[1], coords[0])
+                if bin in bins_to_nghds:
+                    tweet_nghd = bins_to_nghds[bin]
+                else:
+                    tweet_nghd = 'Outside Pittsburgh'
+                if nghd == tweet_nghd:
+                    tweets_per_word[nghd][word].append(row[0])
+            print "done getting tweets for " + word
+
+        print "done getting tweets for " + nghd
+
+    print "done with all neighborhoods"
+    '''
+    print "writing to JSON file"
+
+    with open('outputs/tweets_per_nghdword.json','w') as outfile:
+        json.dump(tweets_per_word,outfile, indent=2)
+
+run_all()
